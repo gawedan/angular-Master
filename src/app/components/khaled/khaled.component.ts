0a9138a6b1c9d698d8290489136f46f9390585b4
gawedan <gawedanmath83@hotmail.com>
2019-06-24 16:36:56 +0200

    Version24.3

--- src/app/components/khaled/khaled.component.ts
+++ src/app/components/khaled/khaled.component.ts
@@ -6,16 +6,12 @@ import {Persons} from './Persons';
   styleUrls: ['./khaled.component.css']
 })
 export class KhaledComponent implements OnInit {
-  person:Persons;
-  people:Persons[];
+person:Persons;
+isKhaled:boolean=true;
+myName:String="Mohammad";
   constructor() {
-    this.person={id:1,name:"khaled",lname:"Mohammad",age:36};
-this.people=[
-{id:1,name:"khaled",lname:"Mohammad",age:3},
-{id:2,name:"Ahmed",lname:"Mohammad",age:4},
-{id:3,name:"khaled",lname:"Mohammad",age:6},
-{id:4,name:"Ahmed",lname:"Mohammad",age:34}
-];
+    this.person = {id:1,name:"khaled",lname:"Mohammad",age:36};
+
      }
 
   ngOnInit() {
