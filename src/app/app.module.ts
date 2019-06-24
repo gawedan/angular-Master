ef399a8d18cbfeb215ae1dbc38e990fce566ae1e
gawedan <gawedanmath83@hotmail.com>
2019-06-24 14:59:36 +0200

    Version24.1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,32 +1,17 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { RouterModule } from '@angular/router';
-import { ReactiveFormsModule } from '@angular/forms';
-
 import { AppComponent } from './app.component';
-import { TopBarComponent } from './top-bar/top-bar.component';
-import { ProductListComponent } from './product-list/product-list.component';
+import { KhaledComponent } from './components/khaled/khaled.component';
 
 @NgModule({
-  imports: [
-    BrowserModule,
-    ReactiveFormsModule,
-    RouterModule.forRoot([
-      { path: '', component: ProductListComponent },
-    ])
-  ],
-  declarations: [
+   declarations: [
     AppComponent,
-    TopBarComponent,
-    ProductListComponent
+    KhaledComponent
   ],
+  imports: [
+    BrowserModule
+    ],
+ providers:[],
   bootstrap: [ AppComponent ]
 })
 export class AppModule { }
-
-
-/*
-Copyright Google LLC. All Rights Reserved.
-Use of this source code is governed by an MIT-style license that
-can be found in the LICENSE file at http://angular.io/license
-*/
---
+++ src/app/components/khaled/khaled.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-khaled',
+  templateUrl: './khaled.component.html',
+  styleUrls: ['./khaled.component.css']
+})
+export class KhaledComponent implements OnInit {
+name = "Khaled";
+lname="Mohammad";
+age=4;
+employee={name:"Khaled",lname:"Mohammad",age:36};
+  constructor() { 
+    this.name="Ahmed";
+    this.changeName();
+    this.changeAge();
+  }
+
+  ngOnInit() {
+    this.age=3;
+  }
+  changeName(){
+    this.name="Ahmed";
+  }
+   changeAge(){
+    this.employee.age=31;
+  }
+}
