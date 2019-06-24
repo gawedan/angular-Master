b40e7dfe933f39302540b472e2575de18e3985f9
gawedan <gawedanmath83@hotmail.com>
2019-06-24 16:55:35 +0200

    Version24.4

--- src/app/components/khaled/khaled.component.ts
+++ src/app/components/khaled/khaled.component.ts
@@ -6,14 +6,7 @@ import {Persons} from './Persons';
   styleUrls: ['./khaled.component.css']
 })
 export class KhaledComponent implements OnInit {
-person:Persons;
-isKhaled:boolean=true;
-myName:String="Mohammad";
-  constructor() {
-    this.person = {id:1,name:"khaled",lname:"Mohammad",age:36};
-
-     }
-
-  ngOnInit() {
-  }
+myImage:string="https://img.purch.com/w/660/aHR0cDovL3d3dy5saXZlc2NpZW5jZS5jb20vaW1hZ2VzL2kvMDAwLzEwNC84MzAvb3JpZ2luYWwvc2h1dHRlcnN0b2NrXzExMTA1NzIxNTkuanBn";
+  constructor() {}
+  ngOnInit() {}
  }
