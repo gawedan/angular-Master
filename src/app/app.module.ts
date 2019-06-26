1e0cfadfa6f5bc2969e077896f9a5c790cfb861e
gawedan <gawedanmath83@hotmail.com>
2019-06-26 16:29:23 +0200

    Version26.3

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,23 +1,35 @@
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { RouterModule, Routes } from '@angular/router';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
-import { DataServiceService } from './service/data-service.service';
-import { MyComponentComponent } from './components/my-component/my-component.component';
 import { HttpModule } from '@angular/http';
-import { MyFormComponent } from './components/my-form/my-form.component';
+import { HomeComponent } from './components/home/home.component';
+import { ProfilesComponent } from './components/profiles/profiles.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { UserprofileComponent } from './components/userprofile/userprofile.component';
+
+const appRoutes:Routes = [
+  {path:'', component:HomeComponent},
+  {path:'profiles', component:ProfilesComponent},
+  {path:'profile/:id', component:UserprofileComponent }
+];
+
 @NgModule({
-   declarations: [
+  declarations: [
     AppComponent,
-    MyComponentComponent,
-    MyFormComponent
+    HomeComponent,
+    ProfilesComponent,
+    NavbarComponent,
+    UserprofileComponent
   ],
   imports: [
     BrowserModule,
     HttpModule,
+    RouterModule.forRoot(appRoutes),
     FormsModule
-    ],
- providers:[DataServiceService],
-  bootstrap: [ AppComponent ]
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
 })
 export class AppModule { }
