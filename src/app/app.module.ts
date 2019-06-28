05603c7be6c949fce2c5522480a0ebddfc31994f
gawedan <gawedanmath83@hotmail.com>
2019-06-28 15:33:17 +0200

    version28_1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,33 +1,16 @@
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
 import { RouterModule, Routes } from '@angular/router';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { HttpModule } from '@angular/http';
-import { HomeComponent } from './components/home/home.component';
-import { ProfilesComponent } from './components/profiles/profiles.component';
-import { NavbarComponent } from './components/navbar/navbar.component';
-import { UserprofileComponent } from './components/userprofile/userprofile.component';
-
-const appRoutes:Routes = [
-  {path:'', component:HomeComponent},
-  {path:'profiles', component:ProfilesComponent},
-  {path:'profile/:id', component:UserprofileComponent }
-];
 
 @NgModule({
   declarations: [
-    AppComponent,
-    HomeComponent,
-    ProfilesComponent,
-    NavbarComponent,
-    UserprofileComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
-    HttpModule,
-    RouterModule.forRoot(appRoutes),
-    FormsModule
+    HttpModule
   ],
   providers: [],
   bootstrap: [AppComponent]
